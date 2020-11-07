49bbbb4129b412f8566428d9845c98b47157955c
EzgiUysal <57458821+EzgiUysal@users.noreply.github.com>
2020-11-07 15:22:48 +0300

    Add files via upload

---
+++ hesap mak.py
@@ -0,0 +1,22 @@
+while True:
+    
+    a=int(input("birinci sayıyı giriniz: "))
+    b=int(input("ikinci sayıyı giriniz: "))
+    c=input("operatör giriniz: ")
+    if c == "+":
+        print( " iki sayını toplamı : " , a+b)
+    elif c == "-" :
+        print( "iki sayını farkı : " ,a-b)
+    elif c== "/" :
+        print( " İki sayının bölümü : " , a/b)
+    elif c== "*" :
+        print( " İki sayının çarpımı : ", a*b)
+    else:
+        print("geçersiz operatör girildi.")
+
+
+
+
+
+
+      
